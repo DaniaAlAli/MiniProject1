7ac5f5f941860bef5d50cec150eef8f3d59e1fb2
DaniaAlAli <dania.al-ali@hotmail.com>
2020-07-01 19:49:39 +0300

    Mini Project FInal Attempt

--- src/components/DataItem.js
+++ src/components/DataItem.js
@@ -3,7 +3,11 @@ import React from "react";
 //Style
 import { ArrowPic, Box, InfoAge, InfoDate, InfoDescription } from "../styles";
 
+//data
+import data from "../data";
 const DataItem = (props) => {
+  const dataLength = data.length;
+
   return (
     <div>
       <Box>
@@ -11,7 +15,11 @@ const DataItem = (props) => {
         <InfoDescription>{props.info.description}</InfoDescription>
         <InfoAge>{props.info.age}</InfoAge>
       </Box>
-      <ArrowPic alt={props.info.date} src={props.info.image} />
+      {dataLength === props.info.id ? (
+        ""
+      ) : (
+        <ArrowPic alt={props.info.date} src={props.info.image} />
+      )}
     </div>
   );
 };
